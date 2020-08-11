662af76038c35d36d7f3b84e15789828595ca878
GilwooKim <60811512+GilwooKim@users.noreply.github.com>
2020-08-12 00:51:55 +0900

    Create s045_for를 이용하여 1이 될 때까지 나누고 곱하기.java

---
+++ Java 200 Project/src/kr/co/infopub/chapter/s045/s045_for를 이용하여 1이 될 때까지 나누고 곱하기.java
@@ -0,0 +1,20 @@
+package kr.co.infopub.chapter.s045;
+
+public class EvenOdd {
+	public static void showOddEvenf(int n) {
+		int temp = n;
+		for(;temp!=1;) {
+			if(temp%2==1) {
+				temp = temp*3 + 1;
+			}else {
+				temp/=2;
+			}
+			System.out.println("["+temp+"]");
+		}
+		System.out.println("\n----------");
+	}
+	public static void main(String[] args) {
+		showOddEvenf(122);
+	}
+
+}
