b4314148d11a4ca024c2c9841521ca251d0cf246
GilwooKim <60811512+GilwooKim@users.noreply.github.com>
2020-08-15 23:50:36 +0900

    Create while을 이용하여 서로 다른 세 정수 만들기.java

---
+++ Java 200 Project/src/kr/co/infopub/chapter/s046/while을 이용하여 서로 다른 세 정수 만들기.java
@@ -0,0 +1,17 @@
+package kr.co.infopub.chapter.s046;
+
+public class s046 {
+//break 
+	public static void main(String[] args) {
+		int range = 10;
+		int start = 1;
+		int a = 0, b = 0, c = 0;
+		while(true) {
+			a = (int)(range*Math.random()+start);
+			b = (int)(range*Math.random()+start);
+			c = (int)(range*Math.random()+start);
+			if(a!=b&&b!=c&&c!=a)break;
+		}
+		System.out.printf("%d\t%d\t%d",a,b,c);
+	}
+}
